54b25d7f42c8425ef6f01f531bd426a25c4249f6
Jsirchia <james.sirchia@gmail.com>
2020-07-20 15:42:10 -0700

    First add of all backend files

---
+++ Lambda/counter.py
@@ -0,0 +1,42 @@
+import json
+import os
+import boto3
+
+#print("heres your shit:")
+#os.environ['TABLE_NAME'] = 'testing'
+#print(os.environ["TABLE_NAME"])
+    
+def lambda_handler(event,context):  #event, context
+    table_name = os.environ["TABLE_NAME"]
+    table = boto3.resource("dynamodb").Table(table_name)
+    
+    response = table.update_item(
+        Key={
+            'id': 'Count'
+        },
+        #UpdateExpression="set Amount = Amount + :inc", 
+        UpdateExpression='ADD Amount :inc',  
+        ExpressionAttributeValues={
+            ':inc': 1
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    # response is equal to the DB Count+1 
+    
+    # turn response into a variable
+    responseBody = json.dumps({"visitorCount": int(float(response["Attributes"]["Amount"]))})
+    
+     
+    apiResponse = {
+    "isBase64Encoded": False,
+    "statusCode": 200,
+    "body": responseBody,
+    "headers": {
+        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,OPTIONS" 
+        },
+    }
+
+    # Return api response object
+    return apiResponse
---
+++ Lambda/test_counter.py
@@ -0,0 +1,50 @@
+from moto import mock_dynamodb2
+import boto3
+import os
+import unittest
+
+def aws_credentials():
+    #Mocked AWS Credentials for moto
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+    os.environ['TABLE_NAME'] = 'testing'
+
+@mock_dynamodb2
+class TestLambdaDDB(unittest.TestCase):
+    def test_handler(self):
+        from counter import lambda_handler
+        # Create dynamodb boto3 object
+        dynamodb = boto3.client('dynamodb')
+        # Get dynamodb table name from env
+        ddbTableName = os.environ['TABLE_NAME']
+    
+        # Create mock table
+        dynamodb.create_table(   #took away table= from front 
+          TableName = ddbTableName,
+          BillingMode='PAY_PER_REQUEST',
+          AttributeDefinitions=[
+              {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+              },
+        ],
+          KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            },
+          ]
+        )
+       
+        LambdaResponse = lambda_handler(0,0)
+        print("Lambda response: ", LambdaResponse)
+        self.assertEqual(200, LambdaResponse['statusCode'])
+    
+
+if __name__ == '__main__':
+  aws_credentials()
+  unittest.main()
+  
+  
